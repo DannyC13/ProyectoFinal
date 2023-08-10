83ccd8637dfcd881cabdd28e3207dbe6e6efac67
AndresVargas <andres200436@gmail.com>
2023-08-10 17:13:28 -0600

    Ingresar empleado con validaciones

---
+++ ProyecyoFinal/SolictarDatos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyecyoFinal
+{
+    class SolictarDatos
+    {
+        Validaciones Validaciones = new Validaciones();
+
+
+        public static string SolicitarNombre(string texto)
+        {
+            while (true)
+            {
+                Console.Write("Ingrese su nombre: ");
+                texto = Console.ReadLine();
+                if (!Validaciones.ValidarString(texto))
+                    Console.WriteLine("EL dato tiene cosas invalidas");
+                else
+                    return texto;
+            }
+        }
+        public static string SolicitarApellido(string texto)
+        {
+            while (true)
+            {
+                Console.Write("Ingrese su apellido: ");
+                texto = Console.ReadLine();
+                if (!Validaciones.ValidarString(texto))
+                    Console.WriteLine("EL dato tiene cosas invalidas");
+
+                else
+                    return texto;
+
+            }
+        }
+        public static string SolicitarCedula(string texto)
+        {
+            while (true)
+            {
+                Console.Write("Ingrese su cedula: ");
+                texto = Console.ReadLine();
+                if (!Validaciones.ValidarNumeros(texto))
+                    Console.WriteLine("EL dato tiene cosas invalidas");
+                else
+                    return texto;
+            }
+
+        }
+        public static string SolicitarCorreo(string texto)
+        {
+            while (true)
+            {
+                Console.Write("Ingrese su correo: ");
+                texto = Console.ReadLine();
+                if (!Validaciones.ValidarCorreo(texto))
+                    Console.WriteLine("EL dato tiene cosas invalidas");
+                else
+                    return texto;
+            }
+
+        }
+    }
+}
---
+++ ProyecyoFinal/Validaciones.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProyecyoFinal
+{
+    class Validaciones
+    {
+        public static bool ValidarString(string texto)
+        {
+            string patron = @"^[a-zA-Z]+$";
+
+            Regex regex = new Regex(patron);
+
+            if (regex.IsMatch(texto))
+                return true;
+            else
+                return false;
+
+        }
+        public static bool ValidarApellidos(string texto)
+        {
+            string patron = @"^[a-zA-Z]";
+
+            Regex regex = new Regex(patron);
+
+            if (regex.IsMatch(texto))
+                return true;
+            else
+                return false;
+        }
+        public static bool ValidarNumeros(string texto)
+        {
+            string patron = @"^\d{9}$";
+
+            Regex regex = new Regex(patron);
+
+            if (regex.IsMatch(texto))
+                return true;
+            else
+                return false;
+        }
+        public static bool ValidarCorreo(string texto)
+        {
+            string patron = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$";
+
+            Regex regex = new Regex(patron);
+
+            if (regex.IsMatch(texto))
+                return true;
+            else
+                return false;
+        }
+    }
+}
